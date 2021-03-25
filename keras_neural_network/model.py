41918c4a8d50856a62023da0c40406f4cb8cabbc
manuel.martin@pagofx.com <manuel.martin@pagofx.com>
2021-03-25 15:43:24 +0100

    modify keral model

--- keras_neural_network/model.py
+++ keras_neural_network/model.py
@@ -5,15 +5,18 @@ from io import open
 import csv
 import numpy as np
 import pandas
+import sys
 
 # https://machinelearningmastery.com/tutorial-first-neural-network-python-keras/
 # Charge the csv
-dataNaN = pandas.read_csv('buildings_information.csv', header=0, encoding='latin1')
-data = dataNaN.fillna(value=1).drop(['Distrito','Tipo','URL'],axis=1).to_numpy()
-X = data[:,1:9]
-Y = data[:,0]
-print(X)
-print(Y)
+np.set_printoptions(threshold=sys.maxsize)
+csv_route = 'finalDataset.csv'
+dataNaN = pandas.read_csv(csv_route, header=0, encoding='latin1')
+data = dataNaN.fillna(value=1).drop(['fullDataset.ID','fullDataset.Precio_m2','fullDataset.Distrito','fullDataset.Planta','fullDataset.Tipo'],axis=1).to_numpy()
+X = data[:,2:7]
+Y = data[:,1]
+# print(Y)
+
 model = Sequential()
 model.add(Dense(12, input_dim=5, activation='relu'))
 model.add(Dense(8, activation='relu'))
@@ -21,6 +24,7 @@ model.add(Dense(1, activation='relu'))
 
 # Compile the keras model 
 #  'mean_absolute_error': Computes the mean absolute error between the labels and predictions.
+# optimizer='sgd','rmsprop'
 # https://www.tensorflow.org/api_docs/python/tf/keras/Model
 
 model.compile(loss='mean_squared_logarithmic_error', optimizer='sgd', metrics=['mean_absolute_percentage_error'])
@@ -28,5 +32,7 @@ model.compile(loss='mean_squared_logarithmic_error', optimizer='sgd', metrics=['
 # fit the keras model on the dataset
 model.fit(X, Y, epochs=150, batch_size=10)
 
-accuracy = model.evaluate(X, Y)
-print('Accuracy: %.2f' % (accuracy*100))
+
+predictions = model.predict(X)
+for i in range(20):
+	print('%s => %d (expected %d)' % (X[i].tolist(), predictions[i], Y[i]))
